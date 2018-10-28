46f040bac32e204aae2384d883f586f582c8ce23
Clover_lily <lily_clover6@163.com>
2018-10-28 17:29:44 +0800

    Prototype 原型模式

--- src/main/java/DesignModel/_5Prototype/Prototype.java
+++ src/main/java/DesignModel/_5Prototype/Prototype.java
@@ -5,16 +5,15 @@ import java.io.*;
 /**
  * 本小结会通过对象的复制，进行讲解。
  * 在Java中，复制对象是通过clone()实现的，先创建一个原型类：
- *
  */
 /**
+ * //实现Cloneable接口即可
  * public class Prototype implements Cloneable {
  *
  *     public Object clone() throws CloneNotSupportedException{
  *         Prototype proto = (Prototype) super.clone();
  *         return proto;
  *     }
- *     Date date =new Date();
  * }
  */
 
@@ -38,7 +37,8 @@ public class Prototype implements Cloneable, Serializable{
 
     //深复制
     //将一个对象复制后，不论是基本数据类型还有引用类型，都是重新创建的。简单来说，就是深复制进行了完全彻底的复制，而浅复制不彻底。
-    public Object deepClone() throws IOException,CloneNotSupportedException{
+    //要实现深复制，需要采用流的形式读入当前对象的二进制输入，再写出二进制数据对应的对象。
+    public Object deepClone() throws IOException,ClassNotFoundException{
         //写入当前对象的二进制流
         ByteArrayOutputStream bos= new ByteArrayOutputStream();
         ObjectOutputStream oos=new ObjectOutputStream(bos);
@@ -47,10 +47,25 @@ public class Prototype implements Cloneable, Serializable{
         //读出二进制流产生的新对象
         ByteArrayInputStream bis= new ByteArrayInputStream(bos.toByteArray());
         ObjectInputStream ois=new ObjectInputStream(bis);
-        //return ois.readObject();待修改
-        return new Object();
+        return ois.readObject();
     }
 
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public void setObj(SerializableObject obj) {
+        this.obj = obj;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public SerializableObject getObj() {
+        return obj;
+    }
 }
 
 class SerializableObject implements Serializable{
