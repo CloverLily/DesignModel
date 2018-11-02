4ba96c9901c45ff132279bf67ca37431d3229e5d
Clover_lily <lily_clover6@163.com>
2018-11-02 17:36:03 +0800

    Composite 组合模式

---
+++ src/main/java/DesignModel_23/StructuralModel_7/_11Composite/Tree.java
@@ -0,0 +1,20 @@
+package DesignModel_23.StructuralModel_7._11Composite;
+
+public class Tree {
+
+    TreeNode root = null;
+
+    public Tree(String name){
+        root = new TreeNode(name);
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree("A");
+        TreeNode nodeB = new TreeNode("B");
+        TreeNode nodeC = new TreeNode("C");
+
+        nodeB.add(nodeC);
+        tree.root.add(nodeB);
+        System.out.println("Build the tree finished~");//A->B->C
+    }
+}
---
+++ src/main/java/DesignModel_23/StructuralModel_7/_11Composite/TreeNode.java
@@ -0,0 +1,48 @@
+package DesignModel_23.StructuralModel_7._11Composite;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class TreeNode {
+
+    private String name;
+    private TreeNode parent;
+    private Vector<TreeNode> children = new Vector<>();
+
+    public TreeNode(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    //添加孩子节点
+    public void add(TreeNode node){
+        children.add(node);
+    }
+
+    //删除孩子节点
+    public void remove(TreeNode node){
+        children.remove(node);
+    }
+
+    //取得孩子节点
+    //依次返回孩子节点
+    public Enumeration<TreeNode> getChildren(){
+        return children.elements();
+    }
+}
