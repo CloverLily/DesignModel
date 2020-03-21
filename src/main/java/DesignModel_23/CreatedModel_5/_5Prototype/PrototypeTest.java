a5ed2e2bc0f294e46b79509f7224a792470841ca
Lily <lily_clover6@163.com>
2020-03-21 23:14:54 +0800

    optimized project structure;

--- src/main/java/DesignModel_23/CreatedModel_5/_5Prototype/PrototypeTest.java
+++ src/main/java/DesignModel_23/CreatedModel_5/_5Prototype/PrototypeTest.java
@@ -15,12 +15,25 @@ public class PrototypeTest {
 
             //浅复制
             Prototype prototype1 = (Prototype) prototype.clone();
-            System.out.println("jsonString:"+prototype1.toJsonString());
-            System.out.println("string:"+prototype1.toString());
+            System.out.println("clone-jsonString:"+prototype1.toJsonString());
+            System.out.println("clone-string:"+prototype1.toString());
             //深复制
             Prototype prototype2 = (Prototype) prototype.deepClone();
-            System.out.println("jsonString:"+prototype2.toJsonString());
-            System.out.println("string:"+prototype2.toString());
+            System.out.println("deepClone-jsonString:"+prototype2.toJsonString());
+            System.out.println("deepClone-string:"+prototype2.toString());
+
+            System.out.println("------update prototype------");
+            //修改原类型
+            SerializableObject serializableObject2 = new SerializableObject();
+            serializableObject2.setDesc("serializableObject2");
+            prototype.setObj(serializableObject2);
+            prototype.setString("prototype2");
+
+            System.out.println("clone-jsonString:"+prototype1.toJsonString());
+            System.out.println("clone-string:"+prototype1.toString());
+            System.out.println("deepClone-jsonString:"+prototype2.toJsonString());
+            System.out.println("deepClone-string:"+prototype2.toString());
+
         } catch(ClassNotFoundException e){
             System.out.println("ClassNotFoundException");
         } catch (IOException e){
