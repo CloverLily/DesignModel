790d0919eade763627c4ff86909879263044c813
Lily <lily_clover6@163.com>
2020-03-26 23:38:02 +0800

    optimized project structure;

--- src/main/java/DesignModel_23/BehavioralModel_11/_13Strategy/StrategyTest13.java
+++ src/main/java/DesignModel_23/BehavioralModel_11/_13Strategy/StrategyTest13.java
@@ -1,20 +1,24 @@
 package DesignModel_23.BehavioralModel_11._13Strategy;
 
 import DesignModel_23.BehavioralModel_11._13Strategy.impl.Minus13;
+import DesignModel_23.BehavioralModel_11._13Strategy.impl.Multiply13;
 import DesignModel_23.BehavioralModel_11._13Strategy.impl.Plus13;
 
 public class StrategyTest13 {
     public static void main(String[] args) {
         String exp1 = "7+9";
         ICalculator cal = new Plus13();
-        int plusResult1 = cal.calculate(exp1);
-        System.out.println(plusResult1);
+        int plusResult = cal.calculate(exp1);
+        System.out.println(plusResult);
 
         String exp2 = "7-6";
         ICalculator cal2 = new Minus13();
-        int plusResult2 = cal2.calculate(exp2);
-        System.out.println(plusResult2);
+        int minusResult = cal2.calculate(exp2);
+        System.out.println(minusResult);
 
-
+        String exp3 = "3*5";
+        ICalculator cal3 = new Multiply13();
+        int multiResult = cal3.calculate(exp3);
+        System.out.println(multiResult);
     }
 }
